d719a0dc8d8fd777bbc36bebcb1eaa04ddabaa20
kyle <kyle0349@126.com>
2018-12-15 09:28:04 +0800

    update

--- hive/src/main/java/com/kyle/impala/ImpalaTest.java
+++ hive/src/main/java/com/kyle/impala/ImpalaTest.java
@@ -1,11 +1,33 @@
 package com.kyle.impala;
 
+import java.sql.*;
+
 public class ImpalaTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         System.out.println("123");
 
-    }
+        String driver = "org.apache.hive.jdbc.HiveDriver";
+        String url = "jdbc:hive2://centos1:21050/;auth=noSasl";
+        String username = "";
+        String password = "";
+        Connection conn = null;
+        Class.forName(driver);
+        conn = (Connection) DriverManager.getConnection(url, username, password);
 
+
+        String sql = "DESCRIBE test.product";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        int col = rs.getMetaData().getColumnCount();
+        System.out.println("=====================================");
+        while (rs.next()) {
+            for (int i = 1; i <= col; i++) {
+                System.out.print(rs.getString(i) + "\t");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("=====================================");
+    }
 }
