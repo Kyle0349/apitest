4898bb8b64fa0152798916a3104499e16bcbb9b8
Kyle0349 <kyle0349@126.com>
2019-01-20 16:03:21 +0800

    update

--- hive/src/main/java/com/kyle/hive/HiveTest01.java
+++ hive/src/main/java/com/kyle/hive/HiveTest01.java
@@ -1,11 +1,36 @@
 package com.kyle.hive;
 
+import java.sql.*;
+
 public class HiveTest01 {
 
-    public static void main(String[] args) {
+    private static String driver = "org.apache.hive.jdbc.HiveDriver";
+    private static String url = "jdbc:hive2://cdh01:10000";
+    //private static String url = "jdbc:hive2://172.19.23.153:11000";
+    private static String username = "hive";
+    private static String password = "";
+    private static Connection conn = null;
 
-        System.out.println("123");
+    static {
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    public void queryDistinct() throws SQLException {
+        String sql = "select count(distinct(product_key)) from fact_order";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.execute("set mapreduce.job.name=test_hive_jdbc");
+        ps.execute("use dim_kylin");
+        ResultSet rs = ps.executeQuery();
+        int col = rs.getMetaData().getColumnCount();
+        System.out.println("=====================================");
     }
 
 
